17cba7a3e3051792a18eab969b26785e9c8d58d3
Jero Pluy <56591617+JeroPlay@users.noreply.github.com>
2024-07-25 22:32:02 +0200

    format the files and added default values to the timestamp properties

--- schema/database_creation.sql
+++ schema/database_creation.sql
@@ -1,170 +1,132 @@
 -- AUTOMATION
 /* basic information of automations */
-
-CREATE TABLE automation
-(
-    a_id          INTEGER PRIMARY KEY AUTOINCREMENT,
-    a_name        TEXT NOT NULL,
-    created       TEXT NOT NULL, -- Using TEXT for datetime
-    autom_mode    INTEGER NOT NULL,
+CREATE TABLE automation (
+    a_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    a_name TEXT NOT NULL,
+    created TEXT NOT NULL DEFAULT current_timestamp,
+    -- Using TEXT for datetime
+    autom_mode INTEGER NOT NULL,
     max_instances INTEGER NOT NULL,
-    script        TEXT NOT NULL,
-    error         INTEGER
+    script TEXT NOT NULL,
+    error INTEGER
 );
-
-
 -- INFORMATION
 /* additional information for automations */
-
-CREATE TABLE additional_information
-(
-    info_id   INTEGER PRIMARY KEY AUTOINCREMENT,
+CREATE TABLE additional_information (
+    info_id INTEGER PRIMARY KEY AUTOINCREMENT,
     info_type TEXT NOT NULL,
-    info      TEXT NOT NULL,
-    a_id      INTEGER NOT NULL,
+    info TEXT NOT NULL,
+    a_id INTEGER NOT NULL,
     FOREIGN KEY (a_id) REFERENCES automation (a_id)
 );
-
 -- INTEGRATION
 /* integration (user generated or from Home Assistant) */
-
-CREATE TABLE integration
-(
-    i_id   INTEGER PRIMARY KEY AUTOINCREMENT,
+CREATE TABLE integration (
+    i_id INTEGER PRIMARY KEY AUTOINCREMENT,
     i_name TEXT NOT NULL
 );
-
 -- ENTITY
 /* represents entities from Home Assistant */
-
-CREATE TABLE entity
-(
-    e_id   INTEGER PRIMARY KEY AUTOINCREMENT,
+CREATE TABLE entity (
+    e_id INTEGER PRIMARY KEY AUTOINCREMENT,
     e_name TEXT NOT NULL,
-    i_id   INTEGER NOT NULL,
+    i_id INTEGER NOT NULL,
     FOREIGN KEY (i_id) REFERENCES integration (i_id)
 );
-
 -- POSSIBLE VALUE
 /* possible values of the integrations */
-
-CREATE TABLE possible_values
-(
-    pv_id       INTEGER PRIMARY KEY AUTOINCREMENT,
-    property    TEXT NOT NULL,    
-    p_value     NUMERIC NOT NULL
+CREATE TABLE possible_values (
+    pv_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    property TEXT NOT NULL,
+    p_value NUMERIC NOT NULL
 );
-
 -- INTEGRATION VALUE
 /* values of the integrations */
-
-CREATE TABLE integration_values 
-(
-    i_id    INTEGER NOT NULL,
-    pv_id   INTEGER NOT NULL,
+CREATE TABLE integration_values (
+    i_id INTEGER NOT NULL,
+    pv_id INTEGER NOT NULL,
     PRIMARY KEY (i_id, pv_id),
     FOREIGN KEY (i_id) REFERENCES integration(i_id),
     FOREIGN KEY (pv_id) REFERENCES possible_values(pv_id)
 );
-
 -- AUTOMATION ENTITY
 /* parameter of automations */
-
-CREATE TABLE automation_entity
-(
-    a_id      INTEGER NOT NULL,
-    e_id      INTEGER NOT NULL,
-    p_role    INTEGER NOT NULL,
-    position  INTEGER NOT NULL,
-    exp_val   NUMERIC NOT NULL,
+CREATE TABLE automation_entity (
+    a_id INTEGER NOT NULL,
+    e_id INTEGER NOT NULL,
+    p_role INTEGER NOT NULL,
+    position INTEGER NOT NULL,
+    exp_val NUMERIC NOT NULL,
     PRIMARY KEY (a_id, e_id, p_role, position),
     FOREIGN KEY (a_id) REFERENCES automation (a_id),
     FOREIGN KEY (e_id) REFERENCES entity (e_id)
 );
-
 -- TEST CASE
 /* information to test cases of automations */
-
-CREATE TABLE test_case
-(
-    case_id         INTEGER PRIMARY KEY AUTOINCREMENT,
-    c_timestamp     TEXT NOT NULL, -- Using TEXT for datetime
-    requirement     NUMERIC,
-    case_priority   INTEGER,
-    a_id            INTEGER NOT NULL,
+CREATE TABLE test_case (
+    case_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    c_timestamp TEXT NOT NULL DEFAULT current_timestamp,
+    -- Using TEXT for datetime
+    requirement NUMERIC,
+    case_priority INTEGER,
+    a_id INTEGER NOT NULL,
     FOREIGN KEY (a_id) REFERENCES automation (a_id)
 );
-
 -- TEST CASE INPUT
 /* input values for test cases */
-
-CREATE TABLE test_case_input
-(
+CREATE TABLE test_case_input (
     case_input_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    test_value    NUMERIC NOT NULL,
-    case_id       INTEGER NOT NULL,
-    a_id          INTEGER NOT NULL,
-    e_id          INTEGER NOT NULL,
-    p_role        INTEGER NOT NULL,
-    position      INTEGER NOT NULL,
+    test_value NUMERIC NOT NULL,
+    case_id INTEGER NOT NULL,
+    a_id INTEGER NOT NULL,
+    e_id INTEGER NOT NULL,
+    p_role INTEGER NOT NULL,
+    position INTEGER NOT NULL,
     FOREIGN KEY (case_id) REFERENCES test_case (case_id),
     FOREIGN KEY (a_id, e_id, p_role, position) REFERENCES automation_entity (a_id, e_id, p_role, position)
 );
-
 -- TEST CASE COLLECTION
 /* saved selection of test cases */
-
-CREATE TABLE test_case_collection
-(
-    tcc_id   INTEGER PRIMARY KEY AUTOINCREMENT,
+CREATE TABLE test_case_collection (
+    tcc_id INTEGER PRIMARY KEY AUTOINCREMENT,
     tcc_name TEXT NOT NULL,
-    a_id     INTEGER NOT NULL,
+    a_id INTEGER NOT NULL,
     FOREIGN KEY (a_id) REFERENCES automation (a_id)
 );
-
 -- TEST CASE COLLECTION TEST CASE
 /* test cases of the selection of test cases */
-
-CREATE TABLE case_collection_test_cases
-(
-    tcc_id  INTEGER NOT NULL,
+CREATE TABLE case_collection_test_cases (
+    tcc_id INTEGER NOT NULL,
     case_id INTEGER NOT NULL,
     PRIMARY KEY (tcc_id, case_id),
     FOREIGN KEY (tcc_id) REFERENCES test_case_collection (tcc_id),
     FOREIGN KEY (case_id) REFERENCES test_case (case_id)
 );
-
 -- TEST EXECUTION
 /* information of the test executions */
-
-CREATE TABLE test_execution
-(
-    te_id          INTEGER PRIMARY KEY AUTOINCREMENT,
-    ex_timestamp   TEXT NOT NULL, -- Using TEXT for datetime
-    exec_mode      TEXT NOT NULL,
-    exec_group     INTEGER NOT NULL,
-    error_type     TEXT,
-    error_comment  TEXT,
-    case_id        INTEGER NOT NULL,
-    a_id           INTEGER NOT NULL,
+CREATE TABLE test_execution (
+    te_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    ex_timestamp TEXT NOT NULL DEFAULT current_timestamp,
+    -- Using TEXT for datetime
+    exec_mode TEXT NOT NULL,
+    exec_group INTEGER NOT NULL,
+    error_type TEXT,
+    error_comment TEXT,
+    case_id INTEGER NOT NULL,
+    a_id INTEGER NOT NULL,
     FOREIGN KEY (case_id) REFERENCES test_case (case_id),
     FOREIGN KEY (a_id) REFERENCES automation (a_id)
 );
-
-
 -- TEST EXECUTION OUTPUT
 /* output parameter of the execution */
-
-CREATE TABLE test_execution_output
-(
-    teo_id      INTEGER PRIMARY KEY AUTOINCREMENT,
-    output_val  NUMERIC,
-    te_id       INTEGER NOT NULL,
-    a_id        INTEGER NOT NULL,
-    e_id        INTEGER NOT NULL,
-    p_role      INTEGER NOT NULL,
-    position    INTEGER NOT NULL,
+CREATE TABLE test_execution_output (
+    teo_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    output_val NUMERIC,
+    te_id INTEGER NOT NULL,
+    a_id INTEGER NOT NULL,
+    e_id INTEGER NOT NULL,
+    p_role INTEGER NOT NULL,
+    position INTEGER NOT NULL,
     FOREIGN KEY (te_id) REFERENCES test_execution (te_id),
     FOREIGN KEY (a_id, e_id, p_role, position) REFERENCES automation_entity (a_id, e_id, p_role, position)
 );
-
--- schema/insert_example_automation_data.sql
+++ schema/insert_example_automation_data.sql
@@ -1,35 +1,45 @@
-INSERT INTO automation(a_name, created, autom_mode, max_instances, script, error) 
-VALUES ('test_automation_123', CURRENT_TIMESTAMP, 0, 1, 'test_automation_123.py', 0);
-
+INSERT INTO automation(
+        a_name,
+        created,
+        autom_mode,
+        max_instances,
+        script,
+        error
+    )
+VALUES (
+        'test_automation_123',
+        CURRENT_TIMESTAMP,
+        0,
+        1,
+        'test_automation_123.py',
+        0
+    );
 INSERT INTO entity(e_id, e_name, i_id)
 VALUES (1, 'binary_sensor.testsensor2', 2);
-
 INSERT INTO entity(e_id, e_name, i_id)
 VALUES (2, 'sun', 41);
-
 INSERT INTO entity(e_id, e_name, i_id)
 VALUES (3, 'sensor.example3', 25);
-
 INSERT INTO automation_entity(a_id, e_id, p_role, position, exp_val)
 VALUES (1, 1, 1, 0, 5);
-
 INSERT INTO automation_entity(a_id, e_id, p_role, position, exp_val)
 VALUES (1, 2, 1, 1, 20);
-
 INSERT INTO automation_entity(a_id, e_id, p_role, position, exp_val)
 VALUES (1, 3, 1, 2, 10);
-
-SELECT a.a_name, e.e_name, i.i_name, p_role, position 
+SELECT a.a_name,
+    e.e_name,
+    i.i_name,
+    p_role,
+    position
 FROM automation_entity
-JOIN entity AS e ON automation_entity.e_id = e.e_id
-JOIN integration AS i ON e.i_id = i.i_id
-JOIN automation AS a ON automation_entity.a_id = a.a_id
+    JOIN entity AS e ON automation_entity.e_id = e.e_id
+    JOIN integration AS i ON e.i_id = i.i_id
+    JOIN automation AS a ON automation_entity.a_id = a.a_id
 WHERE a.a_id == 1;
-
 SELECT pos_val.*
 FROM automation_entity AS ae
-JOIN entity AS e ON ae.e_id = e.e_id
-JOIN integration AS i ON e.i_id = i.i_id
-JOIN integration_values AS iv ON i.i_id = iv.i_id
-JOIN possible_values AS pos_val ON  iv.pv_id = pos_val.pv_id
+    JOIN entity AS e ON ae.e_id = e.e_id
+    JOIN integration AS i ON e.i_id = i.i_id
+    JOIN integration_values AS iv ON i.i_id = iv.i_id
+    JOIN possible_values AS pos_val ON iv.pv_id = pos_val.pv_id
 WHERE ae.position == 0;
