866c1a82b68db5dfded41d355d35ccc594bbbb4c
Jero Pluy <56591617+JeroPlay@users.noreply.github.com>
2024-08-06 16:00:20 +0200

    set the package_dir and where attribute for the setup

--- src/setup.py
+++ src/setup.py
@@ -1,9 +1,12 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='backend',
+    name='test_environment_project',
     version='0.0.1',
-    packages=find_packages(),
+    description='Contains all the code for the test environment application.',
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
+    namespace_packages=['automation_test_environment'],
     install_requires=[
         'Jinja2==3.1.4',
         'MarkupSafe==2.1.5',
@@ -11,14 +14,6 @@ setup(
         'slugify==0.0.1',
         'voluptuous==0.15.2',
         'voluptuous-serialize==2.6.0',
-    ],
-) 
-
-setup(
-    name='frontend',
-    version='0.0.1',
-    packages=find_packages(),
-    install_requires=[
         'customtkinter==5.2.2',
         'darkdetect==0.8.0',
         'packaging==24.1',
