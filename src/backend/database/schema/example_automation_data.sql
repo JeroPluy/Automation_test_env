1c2a0d807fbc1f32f6ca2e8a0b426d9a0c3b7ccb
Jero Pluy <56591617+JeroPlay@users.noreply.github.com>
2024-08-14 00:13:09 +0200

    added new example automation if the database is initialized

--- src/backend/database/schema/example_automation_data.sql
+++ src/backend/database/schema/example_automation_data.sql
@@ -1,6 +1,5 @@
 INSERT INTO automation(
         a_name,
-        created,
         autom_mode,
         max_instances,
         script,
@@ -8,7 +7,6 @@ INSERT INTO automation(
     )
 VALUES (
         'test_automation_123',
-        CURRENT_TIMESTAMP,
         0,
         1,
         'test_automation_123.py',
@@ -24,8 +22,9 @@ INSERT INTO automation_entity(a_id, e_id, p_role, position, exp_val)
 VALUES (1, 1, 1, 0, 5);
 INSERT INTO automation_entity(a_id, e_id, p_role, position, exp_val)
 VALUES (1, 2, 1, 1, 20);
-INSERT INTO automation_entity(a_id, e_id, p_role, position, exp_val)
-VALUES (1, 3, 1, 2, 10);
+INSERT INTO automation_entity(a_id, e_id, p_role, position, exp_val, parent)
+VALUES (1, 3, 1, 2, 10, 1);
+
 SELECT a.a_name,
     e.e_name,
     i.i_name,
@@ -36,6 +35,7 @@ FROM automation_entity
     JOIN integration AS i ON e.i_id = i.i_id
     JOIN automation AS a ON automation_entity.a_id = a.a_id
 WHERE a.a_id == 1;
+
 SELECT pos_val.*
 FROM automation_entity AS ae
     JOIN entity AS e ON ae.e_id = e.e_id
--- src/backend/database/src/__init__.py
+++ src/backend/database/src/__init__.py
@@ -2,17 +2,29 @@
 This package is responsible for the database handling of the automation test environment.
 """
 
-from backend.utils.env_const import DATABASE, INIT_FILE, INTEG_DATA
+from backend.utils.env_const import (
+    DATABASE,
+    INIT_FILE,
+    INTEG_DATA,
+    EXAMPLE_AUTOMATION_PATH,
+)
+from backend.automation_gen.automation_creation import load_new_automation_data
 
 from os import path
 import sqlite3 as sqlite
 
+from .db_create_autom import add_automation
+
 
 def _load_data_foundation():
-    """load the standard integration names and their possible values into the database
+    """
+    load the standard integration names and their possible values into the database
+    and add an example automation to the database
 
     needed sql-Files:
         -   schema/standard_integration.sql
+    needed yaml-Files:
+        -   test_data/yaml_files/example_automations/turn_off_living_room_main_light.yaml
     """
     with open(INTEG_DATA) as integration_data:
         standard_integration_data = integration_data.read()
@@ -27,6 +39,13 @@ def _load_data_foundation():
             con.commit()
     except sqlite.IntegrityError as e:
         print(str(e) + " - data already loaded")
+
+    # add an example automation to the database
+    add_automation(
+        load_new_automation_data(
+            path.join(EXAMPLE_AUTOMATION_PATH, "turn_off_living_room_main_light.yaml")
+        )
+    )
 
 
 def init_db():
@@ -54,6 +73,10 @@ def init_db():
 
             # load base data for the data base
             _load_data_foundation()
+
+            # print success message
+            print("Database initialized successfully")
+
 
 # Trigger the function when the module is imported
 init_db()
