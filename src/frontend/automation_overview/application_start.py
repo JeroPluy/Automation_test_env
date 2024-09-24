afd5982dff0fd78a5b42e600b597bb32f581c8ef
Jero <56591617+JeroPlay@users.noreply.github.com>
2024-09-24 18:37:42 +0200

    added a skip function to load faster in the testing frames

--- src/frontend/automation_overview/application_start.py
+++ src/frontend/automation_overview/application_start.py
@@ -3,12 +3,14 @@ This module is the entry point for the application. It displays the start screen
 """
 
 from os import path
-from frontend.customWidgets import customWidgets as cW
-from frontend import automation_overview as aS
 
 from customtkinter import CTkLabel
 
-from backend.database.db_utils import load_projects, load_automations
+from backend.database.db_utils import load_automations, load_projects
+from frontend import automation_insertion as aI
+from frontend import automation_overview as aS
+from frontend.customWidgets import customWidgets as cW
+
 
 class StartFrame(cW.BasisFrame):
     """
@@ -37,22 +39,38 @@ class StartFrame(cW.BasisFrame):
         app.selected_project = None
         app.projects = load_projects()
 
-        # TODO debug -----
-        print(app.projects)
-        delay = 1000
-        # TODO debug -----
-        
-        self.after(delay, self.next_frame)
+        # # TODO debug -----
+        # print(app.projects)
+        # delay = 1000
+        # self.after(delay, self.next_frame)
+        # # TODO debug -----
 
+        # self.after(100, self.next_frame)
+
+        # TODO debug: skip to automation creation
+        self.after(100, self.skip_to_automation_creation)
 
     def next_frame(self):
         if len(self.app.projects) <= 1:
             if len(self.app.projects) == 0:
-                self.app.projects.append("uncategorized") 
+                self.app.projects.append("uncategorized")
                 self.app.project_automations = []
             if self.app.projects[0] == "uncategorized":
                 self.app.project_automations = load_automations()
                 self.app.load_new_frame(self, aS.AutomationSelectionFrame(self.app))
         else:
             self.app.load_new_frame(self, aS.ProjectSelectionFrame(self.app))
-        
+
+    def skip_to_automation_creation(self):
+        create_frame = self.app.load_new_frame(
+            self, aI.AutomationCreationFrame(self.app)
+        )
+        create_frame.load_automation(
+            automation_path=path.join(
+                "test_data",
+                "yaml_files",
+                "example_automations",
+                "2024.08.02",
+                "living_room_tv_lighting.yaml",
+            ),
+        )
